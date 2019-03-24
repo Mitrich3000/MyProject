8c427e0eade2603a550260b4b3ffcb2595460f1d
Mitrich3000 <doncov@mail.ru>
2019-03-24 18:09:15 +0300

    Setting admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 
 from blog.models import Blog, Post
 
+
+# Define the admin class
+class PostAdmin(admin.ModelAdmin):
+    fields = ('title', 'content')
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(blog__owner__username=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.blog = Blog.objects.get(owner=request.user)
+        obj.save()
+
+
 admin.site.register(Blog)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
